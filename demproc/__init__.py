5129c214d666abe97352437bf38f24eaf8797716
Andrew Lane <ajlane50@gmail.com>
2019-03-05 18:18:09 +0000

    Make setup.py, version as v0.0.1

--- demproc/__init__.py
+++ demproc/__init__.py
@@ -2,6 +2,7 @@
 DEMproc
 ~~~~~~~
 """
+__version__ = "0.0.1"
 from demproc.makelayers import (make_hydro_correct_dem, make_flow_direction_map, 
     make_slope_map, make_continuous_aspect_map, make_binary_aspect_map,
     derive_all)
